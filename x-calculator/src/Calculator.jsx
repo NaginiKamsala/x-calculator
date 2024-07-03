54cf9cdb27a473de0f18558b63e1655c06f83bc7
NaginiKamsala <naginisanju@gmail.com>
2024-07-03 21:01:52 +0530

    change1

--- x-calculator/src/Calculator.jsx
+++ x-calculator/src/Calculator.jsx
@@ -25,52 +25,56 @@ const Calculator = () => {
   };
 
   return (
-    <div className={styles.calculator}>
-      <h1>React Calculator</h1>
-      <input
-        type="text"
-        value={input}
-        readOnly
-        className={styles.calculatorinput}
-      />
-      <input
-        type="text"
-        value={result}
-        readOnly
-        className={styles.calculatorinput}
-      />
-      <div className={styles.calculatorbuttons}>
-        {["7", "8", "9", "+"].map((value) => (
-          <button key={value} onClick={() => handleClick(value)}>
-            {value}
-          </button>
-        ))}
-        {["4", "5", "6", "-"].map((value) => (
-          <button key={value} onClick={() => handleClick(value)}>
-            {value}
-          </button>
-        ))}
-        {["1", "2", "3", "*"].map((value) => (
-          <button key={value} onClick={() => handleClick(value)}>
-            {value}
-          </button>
-        ))}
-        {["C"].map((value) => (
-          <button key={value} onClick={handleClear}>
-            {value}
-          </button>
-        ))}
+    <body>
+      <div className={styles.calculator}>
+        <h1>React Calculator</h1>
+        <input
+          type="text"
+          value={input}
+          readOnly
+          className={styles.calculatorinput}
+        />
+        <input
+          type="text"
+          value={result}
+          readOnly
+          className={styles.calculatorinput}
+        />
+        <div className={styles.calculatorbuttons}>
+          {["7", "8", "9", "+"].map((value) => (
+            <button key={value} onClick={() => handleClick(value)}>
+              {value}
+            </button>
+          ))}
+          {["4", "5", "6", "-"].map((value) => (
+            <button key={value} onClick={() => handleClick(value)}>
+              {value}
+            </button>
+          ))}
+          {["1", "2", "3", "*"].map((value) => (
+            <button key={value} onClick={() => handleClick(value)}>
+              {value}
+            </button>
+          ))}
+          {["C"].map((value) => (
+            <button key={value} onClick={handleClear}>
+              {value}
+            </button>
+          ))}
 
-        {["0", "=", "/"].map((value) => (
-          <button
-            key={value}
-            onClick={value === "=" ? handleCalculate : () => handleClick(value)}
-          >
-            {value}
-          </button>
-        ))}
+          {["0", "=", "/"].map((value) => (
+            <button
+              key={value}
+              onClick={
+                value === "=" ? handleCalculate : () => handleClick(value)
+              }
+            >
+              {value}
+            </button>
+          ))}
+        </div>
       </div>
-    </div>
+    </body>
   );
 };
 
